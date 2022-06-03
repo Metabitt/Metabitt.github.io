27df6704d56ebfd9ad35c15684fa90bb84cc1683
Angel <angelipanake@gmail.com>
2022-06-02 21:01:23 -0500

    updated scripts

--- script.js
+++ script.js
@@ -43,11 +43,12 @@ $(document).ready(function () {
   });
 
   //prevents the submit button from being pressed more than once
-  var submitButton = document.getElementById("submit_form");
-  var form = document.getElementById("email_form");
-  form.addEventListener("submit", function (e) {
-      setTimeout(function() {
-          submitButton.value = "Sending...";
-          submitButton.disabled = true;
+  var submitButton = document.getElementById('submit_form');
+  var form = document.getElementById('email_form');
+  form.addEventListener('submit', function (e) {
+    setTimeout(function () {
+      submitButton.value = 'Sending...';
+      submitButton.disabled = true;
+    }, 100);
   });
 });
