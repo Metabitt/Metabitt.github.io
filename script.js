de24ba1c18c24d9668cb25437cfa02a20c20a46f
Angel <angelipanake@gmail.com>
2023-04-02 13:17:38 -0500

    updated web witj project section, still working

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 $(document).ready(function () {
-  const $navbar = $('.navbar');
-  const $scrollBtn = $('.scroll-up-btn');
-  const $menu = $('.navbar .menu');
-  const $menuBtn = $('.menu-btn');
-  const typed = new Typed('.typing', {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass('sticky');
+    } else {
+      $('.navbar').removeClass('sticky');
+    }
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass('show');
+    } else {
+      $('.scroll-up-btn').removeClass('show');
+    }
+  });
+
+  //slide-up script
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
+  });
+
+  // toggle menu/navbar script
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
+  });
+
+  //typing animation script
+  var typed = new Typed('.typing', {
     strings: ['Portfolio', 'Portafolio', 'CV', 'Curriculum Vitae'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
-  const typed2 = new Typed('.typing-2', {
+  var typed = new Typed('.typing-2', {
     strings: [
       'self-taught programmer',
       'problem solver',
@@ -20,33 +41,4 @@ $(document).ready(function () {
     backSpeed: 60,
     loop: true,
   });
-
-  let scrollTimeout;
-  $(window).on('scroll', function() {
-    clearTimeout(scrollTimeout);
-    scrollTimeout = setTimeout(function() {
-      if (this.scrollY > 20) {
-        $navbar.addClass('sticky');
-      } else {
-        $navbar.removeClass('sticky');
-      }
-      if (this.scrollY > 500) {
-        $scrollBtn.addClass('show');
-      } else {
-        $scrollBtn.removeClass('show');
-      }
-    }, 100); // debounce delay
-
-  });
-
-  $scrollBtn.click(function () {
-    $('html').animate({ scrollTop: 0 });
-  });
-
-  $menuBtn.click(function () {
-    $menu.toggleClass('active');
-    $menuBtn.find('i').toggleClass('active');
-  });
-
 });
-
